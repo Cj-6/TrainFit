ad942d63f2b403462c274da8fb9379fe281d7251
Cj-6 <cjcarrier7@gmail.com>
2024-03-26 21:24:00 -0400

    last sorry :(

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return render_template('nutrition.html')
+    return render_template('index.html')
 
 @app.get('/nutrition')
 def nutrition():
