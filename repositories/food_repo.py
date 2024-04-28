28d24dcb5a187953cfa7b9f803940f82f8496b4d
Cj-6 <cjcarrier7@gmail.com>
2024-04-27 21:26:53 -0400

    added the funciton where you can create a food item

--- repositories/food_repo.py
+++ repositories/food_repo.py
@@ -1,3 +1,4 @@
+import uuid
 from repositories.db import get_pool
 from psycopg.rows import dict_row
 
@@ -21,5 +22,20 @@ def get_food_by_id(food_id):
         with conn.cursor(row_factory=dict_row) as cursor: 
             cursor.execute("SELECT * FROM food WHERE foodid = %s", (food_id,))
             return cursor.fetchone()  # fetch the food with the given ID
+        
+def create_food(food):
+    pool = get_pool()
+    with pool.connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('''
+                            INSERT INTO food 
+                                (foodid, name, calories, protein, total_fat, saturated_fat, trans_fat, 
+                                cholesterol, sodium, carbohydrates, sugars) 
+                            VALUES 
+                                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', 
+                            (str(uuid.uuid4()), food['name'], food['calories'], food['total_fat'],  
+                            food['saturated_fat'], food['trans_fat'], food['cholesterol'], 
+                            food['sodium'], food['carbohydrates'], food['sugars'], food['protein']))
+            conn.commit()
 
 
